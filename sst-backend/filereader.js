87cef77e6d51e33538952224acfaa29871af41d8
daksh123456 <dakshkanaujia@gmail.com>
2024-07-01 09:55:11 +0530

    File Read Commands Jul 1

--- sst-backend/filereader.js
+++ sst-backend/filereader.js
@@ -1,10 +1,44 @@
 const fs = require('fs');
 
 // Asynchronous read
-fs.readFile('file.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading file:');
-        return;
+const data = fs.readFileSync('file.txt', 'utf8');
+
+// fs.readFile('file.txt', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error('Error reading file:');
+//         return;
+//     }
+//     // console.log('File content: is as follows', data);
+// });
+
+// console.log('Asynchronous read: ' + data);
+
+// fs.appendFile('file.txt', 'Hello content!', (err) => {
+//     if (err) {
+//         console.error('Error writing to file:');
+//         return;
+//     }
+    // console.log('File write successful');
+// }
+// );
+
+// append file function
+fs.appendFileSync('file.txt', "class is going on");
+
+// Make Directory Function
+fs.mkdir('dir1', (err) => {
+    if(err){
+        console.log(err);
     }
-    console.log('File content: is as follows', data);
+    console.log("Succesfull")
+})
+
+// Remove Directory Function
+fs.rmdir('dir1', (err) => {
+    console.log("Successfully Removed")
+})
+
+// Rename Function
+fs.rename('file1.txt', 'file.txt', (err) => {
+    console.log("Successfully renamed");
 });
