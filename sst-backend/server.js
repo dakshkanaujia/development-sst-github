6fca73a1efef045797292ab48a59f0d36f2a022f
daksh123456 <dakshkanaujia@gmail.com>
2024-07-01 11:54:58 +0530

    /login loading index.html

--- sst-backend/server.js
+++ sst-backend/server.js
@@ -1,16 +1,36 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs')
+const path = require('path')
+
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html><head><title>node js class </title></head><body>')
-    res.write('<h1> Hello, World!</h1>')
-    res.write('</body></html>')
-    res.end();
-})
+    if(req.url === '/login'){
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, (err, data) => {
+            if(err){
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Internal Server Error');
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        })
+    }else{
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html><head><title>Node.js Class</title></head><body>');
+        res.write('<h1>Hello, World!</h1>');
+        res.write('</body></html>');
+        res.end();
+    }
+});
 
 const port = 3000;
 const host = 'localhost';
 
 server.listen(port, host, () => {
-    console.log(`Server is listening on https://${host}:${port}`);
+    console.log(`Server is listening on http://${host}:${port}`);
 });
+
+// server.listen('/login', )
